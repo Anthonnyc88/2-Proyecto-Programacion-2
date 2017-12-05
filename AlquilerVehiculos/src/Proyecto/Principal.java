64feb7cbac802325102bb4f4006ace1fccc6f4c4
punts hola mundo <anthonnyc10@gmail.com>
2017-12-05 15:44:41 -0600

    main conexion

--- AlquilerVehiculos/src/Proyecto/Principal.java
+++ AlquilerVehiculos/src/Proyecto/Principal.java
@@ -22,7 +22,7 @@ public class Principal {
     public static void main(String[] args) {
 
         System.out.println("Conectado: " + conectando.crearConexionRegistrosAnthonny());
-        //System.out.println("Conectado: " + conectando.crearConexionRegistrosAnthonny());
+        //System.out.println("Conectado: " + conectando.crearConexionRegistrosRoger());
         if (conectando.crearConexionRegistrosAnthonny()) {
             LoginPrincipal ventanaPrincipal = new LoginPrincipal();
             ventanaPrincipal.setVisible(true);
